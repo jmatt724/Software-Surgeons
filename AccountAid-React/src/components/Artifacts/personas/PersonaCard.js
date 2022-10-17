38f143afdb917c4fd90acee87fd9672d47bb7d19
Grant Pennington <grant.pennington@mxdprocess.com>
2022-10-17 14:24:14 -0400

    moved persona components into personas folder

---
+++ AccountAid-React/src/components/Artifacts/personas/PersonaCard.js
@@ -0,0 +1,30 @@
+import { Flex, Heading, Text } from '@chakra-ui/react'
+import React from 'react'
+import PersonaModal from './PersonaModal'
+
+function PersonaCard({ category, artifact }) {
+
+  return (
+    <Flex
+        width={350}
+        height={275}
+        bg={'white'}
+        borderRadius={'md'}
+        boxShadow={'md'}
+        id={artifact.id}
+        direction='column'
+    >
+        <Flex direction='row'>
+          <Heading m={6}>{artifact.name}</Heading>
+        </Flex>
+        <Flex width={300} textAlign={'left'} ml={6}>
+          <Text fontSize={'1.25rem'}>{artifact.outline}</Text>
+        </Flex>
+        <Flex height={50} justify='flex-start' align='center'>
+          <PersonaModal category={category} artifact={artifact}/>
+        </Flex>
+    </Flex>
+  )
+}
+
+export default PersonaCard
---
+++ AccountAid-React/src/components/Artifacts/personas/PersonaModal.js
@@ -0,0 +1,66 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    Button,
+    Text,
+    Heading,
+    Flex,
+  } from '@chakra-ui/react'
+import React from 'react'
+
+function PersonaModal({ category, artifact, persona }) {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+  
+    const handleSizeClick = () => {
+      onOpen()
+    }
+  
+    return (
+      <>
+        <Text mt={3} m={6} color='blue.400' fontSize={'1rem'} 
+            _hover={{ cursor: 'pointer' }} 
+            onClick={() => handleSizeClick()}
+        >
+            {'Read more'}
+        </Text>
+  
+        <Modal onClose={onClose} size={'full'} isOpen={isOpen} id={artifact.id}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>
+                <Heading fontSize={'4rem'}>
+                    {`${category} ${artifact.id} - ${artifact.name}`}
+                </Heading>
+            </ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+                <Flex direction={'row'}>
+                <Flex width={'60%'} lineHeight={'10'} direction='column'>
+                    <Text fontSize={'1.75rem'}>
+                        {artifact.description}
+                    </Text>
+                    <br />
+                    <Text fontSize={'1.75rem'}>
+                        {artifact.idea}
+                    </Text>
+                </Flex>
+                <Flex width={700} height={600} ml={20}>
+                    <img src={process.env.PUBLIC_URL+`images/personas/${artifact.img}`} alt={artifact.name} />
+                </Flex>
+                </Flex>
+            </ModalBody>
+            <ModalFooter>
+              <Button onClick={onClose}>Close</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    )
+}
+export default PersonaModal
