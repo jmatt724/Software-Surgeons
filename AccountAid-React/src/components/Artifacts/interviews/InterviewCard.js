d17904c59c76124a6f67c04b1da86e70f74ace72
Grant Pennington <grant.pennington@mxdprocess.com>
2022-10-17 14:23:45 -0400

    moved interview components into inteviews folder

---
+++ AccountAid-React/src/components/Artifacts/interviews/InterviewCard.js
@@ -0,0 +1,32 @@
+import { Flex, Heading, Text } from '@chakra-ui/react'
+import React from 'react'
+import InterviewModal from './InterviewModal'
+
+function InterviewCard({ artifact }) {
+  //const description = artifact.description.substring()
+
+  return (
+    <Flex
+        width={350}
+        height={175}
+        bg={'white'}
+        borderRadius={'md'}
+        boxShadow={'md'}
+        id={artifact.id}
+        direction='column'
+    >
+        <Flex direction='row'>
+          <Heading m={6}>{artifact.label}</Heading>
+        </Flex>
+        <Flex height={50} justify='flex-start' align='center'>
+          <InterviewModal artifact={artifact}/>
+        </Flex>
+    </Flex>
+  )
+}
+
+/*
+<Text mt={3} m={6} color='blue.400' fontSize={'1rem'} _hover={{ cursor: 'pointer' }}>{'Read more'}</Text> 
+*/
+
+export default InterviewCard
