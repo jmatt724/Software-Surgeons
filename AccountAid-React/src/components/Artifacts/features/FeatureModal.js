735133ec43667ced325ea8d466ed1e21779f7760
Grant Pennington <grant.pennington@mxdprocess.com>
2022-10-21 17:05:21 -0400

    Changed feature modal to handle structure of feature data in constants

--- AccountAid-React/src/components/Artifacts/features/FeatureModal.js
+++ AccountAid-React/src/components/Artifacts/features/FeatureModal.js
@@ -11,6 +11,8 @@ import {
     Text,
     Heading,
     Flex,
+    Box,
+    VStack,
   } from '@chakra-ui/react'
 import React from 'react'
 
@@ -44,9 +46,41 @@ function FeatureModal({ category, artifact, persona }) {
                 <Flex direction={'row'}>
                 <Flex width={'60%'} lineHeight={'10'} direction='column'>
                     {features.map((feature) => 
-                        <Text key={feature.id} fontSize={'2rem'} mt={10}>
-                            {`${feature.label}: ${feature.description}`}
+                      <Box width={1800}>
+                        <Text key={feature.id} fontSize={'2.5rem'} mt={10} mb={6} fontWeight={'bold'}>
+                            {`${feature.label}: `}
                         </Text>
+                        <Flex direction={'row'} spacing={10}>
+                          <Flex direction={'column'} bg={'primary.secondary'} p={10}>
+                          <Text key={feature.id} fontSize={'2rem'} >
+                              {`Description:`}
+                          </Text>
+                          {feature.description.map((desc, index) => 
+                              <Text key={desc} fontSize={'2rem'} mt={10}>
+                                {`${index}. ${desc}`}
+                              </Text>
+                          )}
+                          </Flex>
+                        <Flex direction={'row'} spacing={10} >
+                          <Flex spacing={8} direction={'column'} bg={'primary.snow'} p={10} width={600}>
+                            <Text key={feature.id} fontSize={'2rem'} >
+                              {`Constraints:`}
+                            </Text>
+                            <Text fontSize={'2rem'} mt={10}>
+                                  {`${feature.constraints}`}
+                            </Text>
+                          </Flex>
+                          <Flex spacing={8} direction={'column'} bg={'primary.main'} p={10} width={600}>
+                            <Text key={feature.id} color={'white'} fontSize={'2rem'}>
+                                {`Comments:`}
+                            </Text>
+                            <Text fontSize={'2rem'} color={'white'} mt={10}>
+                                  {`${feature.comments}`}
+                            </Text>
+                          </Flex>
+                        </Flex>
+                        </Flex>
+                      </Box>
                     )}
                 </Flex>
                 </Flex>
