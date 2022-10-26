254ed12eab7f77b6b7f1f0cb7c33addcdff4a726
Grant Pennington <grant.pennington@mxdprocess.com>
2022-10-26 15:25:57 -0400

    added Expenses, sidebar, and YourCards prototype components

---
+++ AccountAid-React/src/components/feature_components/dashboard/SideBar.js
@@ -0,0 +1,11 @@
+import { Flex, Heading } from '@chakra-ui/react'
+import React from 'react'
+
+function SideBar({ label }) {
+  return (
+    <Flex height={'100%'} width={125} bg={'primary.main'}>
+    </Flex>
+  )
+}
+
+export default SideBar
