fbdc8a2bbc67fe1ec987590ff9c8fa3ac5e12f46
Grant Pennington <grant.pennington@mxdprocess.com>
2022-10-26 19:53:07 -0400

    added MyTransactions component to dashboard page

--- AccountAid-React/src/pages/Dashboard.js
+++ AccountAid-React/src/pages/Dashboard.js
@@ -1,15 +1,24 @@
 import { Box, Flex, Heading, Text, VStack } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BALANCE } from '../components/feature_components/bank_balance/balanceData'
 import BalanceDisplay from '../components/feature_components/bank_balance/BalanceDisplay'
 import BalanceStat from '../components/feature_components/bank_balance/BalanceStat'
 import CreditCard from '../components/feature_components/credit_cards/CreditCard'
 import Expenses from '../components/feature_components/dashboard/Expenses'
+import MyTransactions from '../components/feature_components/dashboard/MyTransactions'
 import SideBar from '../components/feature_components/dashboard/SideBar'
 import YourCards from '../components/feature_components/dashboard/YourCards'
-import BudgetStatCard from '../components/feature_components/stats/BudgetStatCard'
+import { useUser } from '../context/UserContext'
+import { calcBalance } from '../data/calculateBalance'
 
 function Dashboard() {
+    const { user, updateBalance } = useUser()
+
+    useEffect(() => {
+        const getBalance = calcBalance(user.cards)
+        updateBalance(getBalance)
+    }, [])
+
     return (
         <Flex width={'100vw'} height={'100vh'} bg={'primary.snow'} direction={'row'}>
             <SideBar />
@@ -17,7 +26,7 @@ function Dashboard() {
                 <Flex height={'20%'} mt={6}>
                     <Flex width={500} height={'100%'} direction={'column'} p={10}>
                         <Heading fontSize={'3rem'}>Dashboard</Heading>
-                        <Text fontSize={'1.5rem'}>Welcome Back, User</Text>
+                        <Text fontSize={'1.5rem'}>{`Welcome Back, ${user.firstName}`}</Text>
                     </Flex>
                     <Flex pt={4}>
                         <BalanceStat />
@@ -27,6 +36,9 @@ function Dashboard() {
                     <Flex>
                         <YourCards />
                     </Flex>
+                </Flex>
+                <Flex height={'42%'} p={4}>
+                    <MyTransactions />
                 </Flex>
             </Flex>
         </Flex>
