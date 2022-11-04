7839a9390f981e63563682e8ac8f089f0911e208
Grant Pennington <grant.pennington@mxdprocess.com>
2022-11-04 14:55:33 -0400

    GP - Added parameter to specify width.

--- AccountAid-React/src/components/feature_components/login/ControlledInput.js
+++ AccountAid-React/src/components/feature_components/login/ControlledInput.js
@@ -1,11 +1,11 @@
 import { Flex, FormHelperText, FormLabel, Input } from '@chakra-ui/react'
 import React, { useState } from 'react'
 
-function ControlledInput({ label, type, value, handleChange }) {
+function ControlledInput({ label, type, value, handleChange, width }) {
     return (
         <Flex direction={'column'} mb={6}>
             <FormLabel>{label}</FormLabel>
-            <Input type={type} value={value} onChange={handleChange}/>
+            <Input type={type} value={value} onChange={handleChange} width={width || '100%'}/>
         </Flex>
     )
   
